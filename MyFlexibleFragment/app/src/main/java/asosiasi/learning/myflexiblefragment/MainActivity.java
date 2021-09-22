d4eeacf7e5bbae14956b1747110c1db20d2d1a89
nandabagus1033 <77679122+nandabagus1033@users.noreply.github.com>
2021-09-22 19:26:31 +0700

    Add files via upload

---
+++ MyFlexibleFragment/app/src/main/java/asosiasi/learning/myflexiblefragment/MainActivity.java
@@ -0,0 +1,21 @@
+package asosiasi.learning.myflexiblefragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+        HomeFragment mHomeFragment = new HomeFragment();
+        mFragmentTransaction.add(R.id.frame_container, mHomeFragment, HomeFragment.class.getSimpleName());
+
+        Log.d("MyFlexibleFragment", "Fragment Name :"+HomeFragment.class.getSimpleName());
+        mFragmentTransaction.commit();
+        }
+}
